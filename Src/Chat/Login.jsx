16558a65175664b4cb42ac2b5f09d2cea937ae53
Sushant Kumar <124777555+Sushant7july@users.noreply.github.com>
2024-08-16 15:37:45 +0530

    Add files via upload

---
+++ Src/Chat/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import './Login.css'
+import assets from '../../assets/assets'
+import {signup} from '../../config/firebase'
+import {login} from '../../config/firebase'
+
+const Login = () => {
+
+  const [currState,setCurrState]=useState("Sign up");
+  const [userName,setUserName]=useState("");
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+
+  const onSubmitHandler =(event) =>{
+      event.preventDefault();
+      if(currState==="Sign up"){
+        signup(userName,email,password);
+      }
+      else{
+        login(email,password);
+      }
+  }
+  
+  return (
+    <div className='Login'>
+       
+      <img src={assets.logo_big} alt="" className='logo'/>
+      <form onSubmit={onSubmitHandler} className='login-form'>
+        <h2>{currState}</h2>
+        {currState==="Sign up" ?<input onChange={(e)=>setUserName(e.target.value)} value={userName} type='text' placeholder='username' className='form-input' required/> : null}
+        <input onChange={(e)=>setEmail(e.target.value)} value={email} type='email' placeholder='Email-Address' className='form-input'required/>
+        <input onChange={(e)=>setPassword(e.target.value)} value={password} type='password' placeholder='Password'  className='form-input' required/>
+        <button type='submit'>{currState==="Sign up" ?"Create account" : "Login now"}</button>
+        <div className='login-term'>
+          <input type='checkbox'/>
+          <p>I  agree to the terms of use and privacy policy.</p>
+        </div>
+        <div className="login-forgot"></div>
+        {currState==="Sign up" ? <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span></p>
+        :<p className="login-toggle">Create an account <span onClick={()=>setCurrState("Sign up")}>Click here</span></p> }
+      </form>
+    </div>
+  )
+}
+
+export default Login
